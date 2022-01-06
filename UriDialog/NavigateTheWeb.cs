7c00f2aa317fbb945ffd7ecb6c0206dee049e2bc
SGrodnik <s.grodnik@genpro.ru>
2022-01-06 22:12:50 +0300

    p90

---
+++ UriDialog/NavigateTheWeb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Petzold.UriDialog {
+    class NavigateTheWeb : Window {
+        Frame f;
+        [STAThread]
+        public static void Main() { new Application().Run(new NavigateTheWeb()); }
+
+        public NavigateTheWeb() {
+            Title = $"({MethodBase.GetCurrentMethod().DeclaringType.Name} Title)";
+            f = new Frame();
+            Content = f;
+            Loaded += OnWindowLoaded;
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e) {
+            UriDialog d = new UriDialog();
+            d.Owner = this;
+            d.Text = "http://";
+            d.ShowDialog();
+            try {
+                f.Source = new Uri(d.Text);
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message, Title);
+            }
+        }
+    }
+}
--- UriDialog/UriDialog.cs
+++ UriDialog/UriDialog.cs
@@ -7,11 +7,33 @@ using System.Windows.Input;
 using System.Windows.Media;
 
 namespace Petzold.UriDialog {
-    internal class UriDialog : Window {
-        [STAThread]
+    class UriDialog : Window {
+        TextBox tb;
         public static void Main() { new Application().Run(new UriDialog()); }
         public UriDialog() {
             Title = $"({MethodBase.GetCurrentMethod().DeclaringType.Name} Title)";
+            ShowInTaskbar = false;
+            SizeToContent = SizeToContent.WidthAndHeight;
+            WindowStyle = WindowStyle.ToolWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            tb = new TextBox();
+            tb.Margin = new Thickness(48);
+            Content = tb;
+
+            tb.Focus();
+        }
+        public string Text {
+            set {
+                tb.Text = value;
+                tb.SelectionStart = tb.Text.Length;
+            }
+            get { return tb.Text; }
+        }
+        protected override void OnKeyDown(KeyEventArgs e) {
+            base.OnKeyDown(e);
+            if (e.Key == Key.Enter) { Close(); }
         }
     }
+
 }
